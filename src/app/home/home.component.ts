1ace31476bc64573933335208102c48c99b20fae
Ayoub <sebbare@micro-systems.it>
2020-01-21 15:19:28 +0100

    Removed unused code

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -30,42 +30,11 @@ export class HomeComponent implements OnInit {
   connectionTest() {
     console.log("test connection");
     this.socketService.testConnection();
-    // this.socket.arrayBuffer2str(299);
-    // this.socket.sendPacket("192.168.1.1", 5555, 1);
-    // this.socket.writeToDevice();
-  }
-
-  sendMessage(message: any) {
-    console.log("sending a message: %1 to the device", message);
-  }
-
-  readMessage() {
-    console.log("reading a message from the device");
-    // this.socket.writeToDevice()
-    // this.socket.str2arrayBuffer("23");
   }
 
   swVersionRequest() {
     console.log("software version request");
-
-    //debugger;
-    if (this.socketService.checkSwVersion()) {
-      this.swVersion = this.socketService.getSwVersion();
-      // this.swVersion = this.socket.swVersionResponse
-      // debugger
-      // setTimeout(() => {
-      //   // this.showSwVersion = true;
-      // }, 2000);
-    } else {
-      setTimeout(() => {
-        alert("Connect to the device first");
-        // this.showSwVersion = false;
-        // this.loadingService.showLoading(false);
-      }, 1000);
-    }
-
-    // will it work?
-    // this.swVersion = this.socket.swVersionResponse
+    this.socketService.getSwVersion()
   }
 
   getTemperature() {
